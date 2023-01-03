9766d5b37d340b11f630c99180e4bccdade06279
Shriram Bhardwaj <121400894+27Shri03@users.noreply.github.com>
2023-01-03 19:45:07 +0530

    Update Calaculator.py

--- Calaculator.py
+++ Calaculator.py
@@ -1,62 +1,116 @@
-import math
-def calci(a,b,c):
-    if c==1:
-        print("A+B = ",a+b)
-    elif c==2:
-        print("A-B = ",a-b)
-    elif c==3:
-        print("A*B = ",a*b)
-    elif c==4:
-        if b==0:
-            print("Division is not defined , try again")
+from tkinter import *
+root = Tk()
+root.geometry('350x400')
+root.title("Calculator")
+
+def click(Event):
+    global calci
+    text = Event.widget.cget("text")
+    if text == "=":
+        if calci.get().isdigit():
+            value = (int(calci.get()))
         else:
-            print("A/B = ",a/b)
-    elif c==5:
-        print("A square",a**2)
-        print("B square",b**2)
-    elif c==6:
-        print("Square root of A: ",math.sqrt(a))
-        print("Square root of B: ",math.sqrt(b))
-    elif c==7:
-        print("A Cube",a**3)
-        print("B Cube",b**3)
-    elif c==8:
-        print("Cube root of A: ",a**(1/3))
-        print("Cube root of B: ",b**(1/3))
-    elif c==9:
-        print("Sin of A: ",math.sin(a))
-        print("Sin of B: ",math.sin(b))
-    elif c==10:
-        g = int(input("Enter base: "))
-        print("Log of A: ",math.log(a,g))
-        print("Log of B: ",math.log(b,g))
-    elif c==11:
-        g = int(input("Enter the power: "))
-        print("A: ",a**g)
-        print("B: ",b**g)        
+            value = eval(calci.get())
+        calci.set(value)
+        ent.update()
+    elif text == "C":
+        calci.set("")
+        ent.update()
     else:
-        print("Invalid operation")
-    
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("5. Square")
-print("6. Square root")
-print("7. Cube")
-print("8. Cube root")
-print("9. Sine")
-print("10. Log")
-print("11. Power")
-print("12. Exit")
+        calci.set(calci.get()+text)
+        ent.update()
 
 
-while(1):
-    a = float(input("Enter First number: "))
-    b = float(input("Enter Second number: "))
-    c = int(input("Enter the operation: "))
-    if c==12:
-        print("Exit successfully")
-        break
-    else:
-        calci(a,b,c)
+calci = StringVar()
+calci.set("")
+
+ent = Entry(root, textvariable=calci, width=40, font=("Arial Bold", 40))
+ent.pack(fill=X, padx=10, pady=10)
+
+f = Frame(bg="pink")
+b = Button(f, text="C", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="9", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="8", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="7", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+f.pack()
+
+f = Frame(bg="pink")
+b = Button(f, text="+", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=12, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="6", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="5", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="4", padx=10, font=("Arial Bold", 20))
+b.pack(side=RIGHT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+f.pack()
+
+
+f = Frame(bg="pink")
+b = Button(f, text="-", padx=12, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=13, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="3", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=9, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="2", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=11, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="1", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=11, pady=10)
+b.bind("<Button-1>", click)
+f.pack()
+
+f = Frame(bg="pink")
+
+
+b = Button(f, text="*", padx=13, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="/", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="%", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+
+b = Button(f, text="=", padx=10, font=("Arial Bold", 20))
+b.pack(side=LEFT, padx=10, pady=10)
+b.bind("<Button-1>", click)
+f.pack()
+
+
+root.mainloop()
+
